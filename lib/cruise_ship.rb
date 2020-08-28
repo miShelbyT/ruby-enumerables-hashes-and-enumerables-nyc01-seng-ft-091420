023006fb90833bbe6ce9864a9c93f23645aaa010
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-28 20:08:00 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,8 +10,8 @@ def select_winner(passengers)
   passengers.each do |suite, person|
     if suite == :suite_a && name.start_with?("A")
       winner = person
+      winner
     end
-    winner
   end
 end
 
