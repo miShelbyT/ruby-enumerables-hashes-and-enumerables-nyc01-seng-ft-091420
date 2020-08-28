5275d9c125df6406292c55aed2fcd1a96c24b8a7
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-28 20:14:37 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,20 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = " "
+  passengers.each do |suite, person|
+    if suite == :suite_a && person.start_with?("A")
+      winner = person
+    end
+  end
+  winner
 end
+
+
+  # add the code snippet here!
